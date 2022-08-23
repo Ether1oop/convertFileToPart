34299415f0ee8f69b155cc5227baceaf4832de26
Ether1oop <1536622592@qq.com>
2022-08-23 09:59:36 +0800

    constant version

--- function.py
+++ function.py
@@ -1,6 +1,82 @@
 import os
 
 
+def verifyPart(strings):
+    string_line = strings.split("\n")
+
+    # 去注释
+    for i in range(0,len(string_line)):
+        line = string_line[i]
+        if line == "":
+            continue
+        flag = line.find("//")
+        if flag != -1:
+            string_line[i] = line[:flag].strip()
+
+    for i in range(0,len(string_line)):
+        front = string_line[i].find("/*")
+        if front != -1:
+            rear = string_line[i][front + 2:].find("*/")
+            if rear != -1:
+                string_line[i] = string_line[i][:front] + string_line[i][rear + 2:]
+            else:
+                string_line[i] = string_line[i][:front]
+                j = i + 1
+                for j in range(i + 1, len(string_line)):
+                    rear = string_line[j].find("*/")
+                    if rear != -1:
+                        string_line[j] = string_line[j][rear + 2:]
+                        break
+                if j < len(string_line):
+                    for k in range(i + 1, j):
+                        string_line[k] = ""
+                else:
+                    for k in range(i + 1, len(string_line)):
+                        string_line[k] = ""
+
+    # 找到emit所在行，向下检索，若遇到‘+’‘-’并且不包含emit的，则返回False，否则返回True
+    emit_line_id = -1
+    for i in range(0,len(string_line)):
+        line = string_line[i]
+        if line == "":
+            continue
+        if line[0] == '+' or line[0] == '-':
+            if line.find(" emit ") != -1:
+                emit_line_id = i
+                break
+
+    if emit_line_id == -1:
+        return False
+
+    for i in range(emit_line_id,len(string_line)):
+        line = string_line[i]
+        if line == "":
+            continue
+        if line[0] == "+" or line[0] == '-':
+            if len(line) == 1:
+                continue
+
+            flag = line.find(" emit ")
+            if flag != -1:
+                _right = line[flag:].find(")")
+                if _right != -1:
+                    continue
+                else:
+                    j = i + 1
+                    for j in range(i + 1, len(string_line)):
+                        _right = string_line[j].find(")")
+                        if _right != -1:
+                            break
+                    if j < len(string_line):
+                        i = j
+                        continue
+                    else:
+                        return True
+            else:
+                return False
+    return True
+
+
 def splitString(strings):
     file_lines = strings.split("\n")
     tags = []
@@ -16,16 +92,25 @@ def splitString(strings):
     tags.append(len(file_lines))
 
     for i in range(0, len(tags) - 1):
-        results.append("\n".join(file_lines[tags[i]: tags[i + 1] - 1]))
+        temp = "\n".join(file_lines[tags[i]: tags[i + 1] - 1])
+        if verifyPart(temp):
+            results.append(temp)
 
     return results
 
 
 def getMaxIdFromList(working_dir):
-    dir_list = os.listdir(working_dir)
-    dir_list.sort(key=lambda x: x[:-4])
+    dir_list = []
+    for item in os.listdir(working_dir):
+        if item == '':
+            continue
+        if item == 'special.txt':
+            continue
+        if os.path.isfile(working_dir + "/" + item):
+            dir_list.append(item)
+    dir_list.sort(key=lambda x: int(x[:-4]))
 
-    for i in range(len(dir_list) - 1, 0, -1):
+    for i in range(len(dir_list) - 1, -1, -1):
         item = dir_list[i]
         if item == "special.txt":
             continue
@@ -54,8 +139,11 @@ def generateNewFiles(absolute_path):
     passages = splitString(fileString)
     url = getUrlFromSpecial(absolute_path)
 
-    if len(passages) <= 1:
+    if len(passages) == 0:
         return False
+
+    with open(absolute_path, "w") as file:
+        file.write(passages[0])
 
     for i in range(1, len(passages)):
         new_id = getMaxIdFromList(working_dir)
@@ -64,8 +152,7 @@ def generateNewFiles(absolute_path):
         with open(working_dir + "/special.txt", "a") as file:
             file.write(url)
 
-    with open(absolute_path, "w") as file:
-        file.write(passages[0])
+
 
     # with open(working_dir + "/" + new_file_name) as file:
     #     file.write(passages)
@@ -73,10 +160,20 @@ def generateNewFiles(absolute_path):
 
 def convertFileToPart(working_dir):
     for root, dirs, files in os.walk(working_dir):
+        if 'special.txt' in files:
+            files.remove("special.txt")
+        files.sort(key=lambda x: int(x[:-4]))
         for item in files:
             generateNewFiles(os.path.join(root, item).replace("\\", "/"))
 
 
-convertFileToPart("special_result")
-# path = 'special_result/EmitChangeParameter/2.txt'
+def randomSelect(working_dir):
+    for root, dirs, files in os.walk(working_dir):
+        for item in files:
+            print(os.path.join(root,item))
+
+# convertFileToPart("special_result")
+# path = 'results/225.txt'
 # generateNewFiles(path)
+# generateNewFiles("results/333.txt")
+randomSelect("results")
